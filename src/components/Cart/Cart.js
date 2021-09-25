826b6b652fb5da4d87f781952d4416ae8461fbf9
Nazmul Hosen <nazmulhosenm668@gmail.com>
2021-09-26 02:49:44 +0600

    cart component style done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,10 +6,10 @@ const Cart = (props) => {
 	let totalAmount = 0;
 	persons.filter((persn) => (totalAmount += Number(persn.worth)));
 	return (
-		<div>
+		<div className="person-cart">
 			<h2>person details</h2>
 			<h3>Person :{persons.length} </h3>
-			<h3>Net worth : ${totalAmount}</h3>
+			<h3>Total amount : ${totalAmount}</h3>
 			{persons.map((person) => (
 				<div className="person-details">
 					<img src={person.img} alt="" className="person-img" />
